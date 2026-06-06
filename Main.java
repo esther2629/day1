7780af1fd28c2fbc3c0ffcd81b80ab9815ea18f0
ESTHER ANANTHA BABU <jasage2012@gmail.com>
2026-06-06 15:55:01 +0530

    add content

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n= sc.nextInt();
+        
+        System.out.println("Hello, World!"+n);
     }
 }
