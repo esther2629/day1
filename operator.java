2d565a1bdbb8f17fe3622a1dd581cd0bbc8617cf
ESTHER ANANTHA BABU <jasage2012@gmail.com>
2026-06-06 15:04:44 +0530

    Initial commit

---
+++ operator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class operator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        // Arithmetic Operators
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Modulus = " + (a % b));
+
+        // Relational Operators
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+
+        // Logical Operators
+        System.out.println("(a > 0 && b > 0) : " + (a > 0 && b > 0));
+        System.out.println("(a > 0 || b > 0) : " + (a > 0 || b > 0));
+        System.out.println("!(a > b) : " + !(a > b));
+
+        sc.close();
+    }
+}
